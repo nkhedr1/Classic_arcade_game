02b98c804f4730f8a3f2ca38f53d3c2b51394e69
Nour Mohammed <noureldinkhedr85@gmail.com>
2018-12-10 10:47:25 -0600

    Concatenated the JS into a single file using gulp-concat

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,92 @@
 "use strict";
 
+var gameLevel = 1;
+var highestScore = 1;
+
+var Enemy = function Enemy(x, y) {
+  this.x = x;
+  this.y = y;
+  this.sprite = 'src/images/enemy-bug.png';
+};
+
+Enemy.prototype.update = function (dt) {
+  if (this.x > 500) {
+    this.x = -200;
+  }
+
+  ;
+  this.x = this.x + gameLevel + Math.floor(Math.random() * 5 + 1) * 40 * dt;
+};
+
+Enemy.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+  if (this.x + 70 > player.x && this.x < player.x + 70 && player.y + 50 > this.y && this.y + 50 > player.y) {
+    player.y = 380;
+    gameLevel = 1;
+    document.getElementById('level-number').innerHTML = "<h2>Level ".concat(gameLevel, "</h2>");
+  }
+
+  ;
+};
+
+function Player(x, y) {
+  this.x = x;
+  this.y = y;
+  this.sprite = 'src/images/char-pink-girl.png';
+}
+
+Player.prototype.update = function (dt) {
+  if (this.y < 40) {
+    this.y = 380;
+
+    if (highestScore === gameLevel) {
+      ++highestScore;
+    }
+
+    ;
+    ++gameLevel;
+    document.getElementById('level-number').innerHTML = "<h2>Level ".concat(gameLevel, "</h2>");
+    document.getElementById('highest-score').innerHTML = "<h2>Highest Score = ".concat(highestScore, "</h2>");
+  }
+};
+
+Player.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}; // conditions for the movement of player
+
+
+Player.prototype.handleInput = function (el) {
+  if (el === 'up' && this.y >= 40) {
+    this.y -= 83;
+  } else if (el === 'down' && this.y < 380) {
+    this.y += 83;
+  } else if (el === 'left' && this.x > 90) {
+    this.x -= 100;
+  } else if (el === 'right' && this.x <= 300) {
+    this.x += 100;
+  }
+
+  this.render();
+};
+
+var allEnemies = [];
+var player = new Player(200, 380);
+var enemy1 = new Enemy(-500, 50);
+var enemy2 = new Enemy(-50, 140);
+var enemy3 = new Enemy(-300, 225);
+allEnemies.push(enemy1, enemy2, enemy3);
+document.addEventListener('keyup', function (e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
+  player.handleInput(allowedKeys[e.keyCode]);
+});
+"use strict";
+
 /* Engine.js
  * This file provides the game loop functionality (update entities and render),
  * draws the initial game board on the screen, and then calls the update and
@@ -180,3 +267,125 @@ var Engine = function (global) {
 
   global.ctx = ctx;
 }(void 0);
+"use strict";
+
+/* Resources.js
+ * This is simply an image loading utility. It eases the process of loading
+ * image files so that they can be used within your game. It also includes
+ * a simple "caching" layer so it will reuse cached images if you attempt
+ * to load the same image multiple times.
+ */
+(function () {
+  var resourceCache = {};
+  var readyCallbacks = [];
+  /* This is the publicly accessible image loading function. It accepts
+   * an array of strings pointing to image files or a string for a single
+   * image. It will then call our private image loading function accordingly.
+   */
+
+  function load(urlOrArr) {
+    if (urlOrArr instanceof Array) {
+      /* If the developer passed in an array of images
+       * loop through each value and call our image
+       * loader on that image file
+       */
+      urlOrArr.forEach(function (url) {
+        _load(url);
+      });
+    } else {
+      /* The developer did not pass an array to this function,
+       * assume the value is a string and call our image loader
+       * directly.
+       */
+      _load(urlOrArr);
+    }
+  }
+  /* This is our private image loader function, it is
+   * called by the public image loader function.
+   */
+
+
+  function _load(url) {
+    if (resourceCache[url]) {
+      /* If this URL has been previously loaded it will exist within
+       * our resourceCache array. Just return that image rather than
+       * re-loading the image.
+       */
+      return resourceCache[url];
+    } else {
+      /* This URL has not been previously loaded and is not present
+       * within our cache; we'll need to load this image.
+       */
+      var img = new Image();
+
+      img.onload = function () {
+        /* Once our image has properly loaded, add it to our cache
+         * so that we can simply return this image if the developer
+         * attempts to load this file in the future.
+         */
+        resourceCache[url] = img;
+        /* Once the image is actually loaded and properly cached,
+         * call all of the onReady() callbacks we have defined.
+         */
+
+        if (isReady()) {
+          readyCallbacks.forEach(function (func) {
+            func();
+          });
+        }
+      };
+      /* Set the initial cache value to false, this will change when
+       * the image's onload event handler is called. Finally, point
+       * the image's src attribute to the passed in URL.
+       */
+
+
+      resourceCache[url] = false;
+      img.src = url;
+    }
+  }
+  /* This is used by developers to grab references to images they know
+   * have been previously loaded. If an image is cached, this functions
+   * the same as calling load() on that URL.
+   */
+
+
+  function get(url) {
+    return resourceCache[url];
+  }
+  /* This function determines if all of the images that have been requested
+   * for loading have in fact been properly loaded.
+   */
+
+
+  function isReady() {
+    var ready = true;
+
+    for (var k in resourceCache) {
+      if (resourceCache.hasOwnProperty(k) && !resourceCache[k]) {
+        ready = false;
+      }
+    }
+
+    return ready;
+  }
+  /* This function will add a function to the callback stack that is called
+   * when all requested images are properly loaded.
+   */
+
+
+  function onReady(func) {
+    readyCallbacks.push(func);
+  }
+  /* This object defines the publicly accessible functions available to
+   * developers by creating a global Resources object.
+   */
+
+
+  window.Resources = {
+    load: load,
+    get: get,
+    onReady: onReady,
+    isReady: isReady
+  };
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
 
 gulp.task('autoprefixer', () =>
     gulp.src('src/css/*.css')
@@ -12,20 +15,23 @@ gulp.task('autoprefixer', () =>
         .pipe(gulp.dest('dist/css'))
 );
 
-const imagemin = require('gulp-imagemin');
-
 gulp.task('imagemin', () =>
     gulp.src('src/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/images'))
 );
 
-const babel = require('gulp-babel');
-
 gulp.task('babel', () =>
     gulp.src('src/js/*.js')
         .pipe(babel({
             presets: ['@babel/env']
         }))
+        .pipe(concat('main.js'))
         .pipe(gulp.dest('dist/js'))
 );
+
+gulp.task('concat', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/js'));
+});
