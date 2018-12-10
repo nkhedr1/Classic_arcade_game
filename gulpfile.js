a8c79807da94612be50d9e63044483da7c352b11
Nour Mohammed <noureldinkhedr85@gmail.com>
2018-12-10 10:57:47 -0600

    Minified the JS using gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
 const imagemin = require('gulp-imagemin');
 const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
 
 gulp.task('autoprefixer', () =>
     gulp.src('src/css/*.css')
@@ -30,8 +32,12 @@ gulp.task('babel', () =>
         .pipe(gulp.dest('dist/js'))
 );
 
-gulp.task('concat', function() {
-  return gulp.src('src/js/*.js')
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/js'));
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('dist/js/main.js'),
+        uglify(),
+        gulp.dest('dist/js')
+    ],
+    cb
+  );
 });
